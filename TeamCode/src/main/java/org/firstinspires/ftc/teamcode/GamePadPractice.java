37193ec338b1c87dedeb47813429cfa41e13f202
Alvin970903 <alvintsao0903@gmail.com>
2025-11-14 23:22:15 -0800

    GamePadPractice
    Learning game pad

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamePadPractice.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class GamePadPractice extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+        // runs 50x+ a second
+        double speedForward = -gamepad1.left_stick_y / 2.0;
+
+        telemetry.addData("x", gamepad1.left_stick_x);
+        telemetry.addData("y", speedForward);
+        telemetry.addData("a", gamepad1.a);
+        telemetry.addData("right x", gamepad1.right_stick_x);
+        telemetry.addData("right y", gamepad1.right_stick_y);
+        telemetry.addData("difference", gamepad1.left_stick_x - gamepad1.right_stick_x);
+    }
+}
