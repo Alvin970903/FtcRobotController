823b31bfc2d0bffa352e9873791a2be10c4238e8
Alvin970903 <alvintsao0903@gmail.com>
2025-11-15 14:04:34 -0800

    DC motor and touch sensor practice

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TouchSensorPractice.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.mechanisms.TestBench;
+
+@TeleOp
+public class TouchSensorPractice extends OpMode {
+    TestBench bench = new TestBench();
+
+    @Override
+    public void init() {
+        bench.init(hardwareMap); // get the touch sensor and set it as input
+    }
+
+    @Override
+    public void loop() {
+        // telemetry.addData("Touch Sensor State", bench.getTouchSensorState());
+        if(bench.getTouchSensorState()){
+            telemetry.addData("Touch Sensor", "Pressed");
+        }
+        else{
+            telemetry.addData("Touch Sensor", "Not Pressed");
+        }
+    }
+}
